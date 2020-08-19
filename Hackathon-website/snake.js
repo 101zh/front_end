137084ffbee9eb62fe7f0513e1543cb426f40255
Hunter Zhuang <hunterzhuang0@gmail.com>
2020-08-19 15:42:27 -0700

    the snake dies when hit wall

--- Hackathon-website/snake.js
+++ Hackathon-website/snake.js
@@ -3,14 +3,14 @@ var context = canvas.getContext('2d');
 
 var grid = 16;
 var count = 0;
-  
+
 var snake = {
   x: 160,
   y: 160,
-  
+
   dx: grid,
   dy: 0,
-  
+
   cells: [],
   maxCells: 4
 };
@@ -30,39 +30,39 @@ function loop() {
   }
 
   count = 0;
-  context.clearRect(0,0,canvas.width,canvas.height);
+  context.clearRect(0, 0, canvas.width, canvas.height);
 
   snake.x += snake.dx;
   snake.y += snake.dy;
 
   if (snake.x < 0) {
-    snake.x = canvas.width - grid;
+    resetGame();
   }
   else if (snake.x >= canvas.width) {
-    snake.x = 0;
+    resetGame();
   }
-  
+
 
   if (snake.y < 0) {
-    snake.y = canvas.height - grid;
+    resetGame();
   }
   else if (snake.y >= canvas.height) {
-    snake.y = 0;
+    resetGame();
   }
 
-  snake.cells.unshift({x: snake.x, y: snake.y});
+  snake.cells.unshift({ x: snake.x, y: snake.y });
 
   if (snake.cells.length > snake.maxCells) {
     snake.cells.pop();
   }
 
   context.fillStyle = 'red';
-  context.fillRect(apple.x, apple.y, grid-1, grid-1);
+  context.fillRect(apple.x, apple.y, grid - 1, grid - 1);
 
-  context.fillStyle='#C39B77';
-  snake.cells.forEach(function(cell, index) {
-    
-    context.fillRect(cell.x, cell.y, grid-1, grid-1);  
+  context.fillStyle = '#C39B77';
+  snake.cells.forEach(function (cell, index) {
+
+    context.fillRect(cell.x, cell.y, grid - 1, grid - 1);
 
     if (cell.x === apple.x && cell.y === apple.y) {
       snake.maxCells++;
@@ -72,26 +72,29 @@ function loop() {
       apple.y = getRandomInt(0, 25) * grid;
     }
     for (var i = index + 1; i < snake.cells.length; i++) {
-      
-  
-      if (cell.x === snake.cells[i].x && cell.y === snake.cells[i].y) {
-        snake.x = 160;
-        snake.y = 160;
-        snake.cells = [];
-        snake.maxCells = 4;
-        snake.dx = grid;
-        snake.dy = 0;
 
-        apple.x = getRandomInt(0, 25) * grid;
-        apple.y = getRandomInt(0, 25) * grid;
+
+      if (cell.x === snake.cells[i].x && cell.y === snake.cells[i].y) {
+        resetGame();
       }
     }
   });
 }
 
+function resetGame() {
+  snake.x = 160;
+  snake.y = 160;
+  snake.cells = [];
+  snake.maxCells = 4;
+  snake.dx = grid;
+  snake.dy = 0;
 
-document.addEventListener('keydown', function(e) {
-  
+  apple.x = getRandomInt(0, 25) * grid;
+  apple.y = getRandomInt(0, 25) * grid;
+}
+
+document.addEventListener('keydown', function (e) {
+
   if (e.which === 37 && snake.dx === 0) {
     snake.dx = -grid;
     snake.dy = 0;
@@ -100,10 +103,10 @@ document.addEventListener('keydown', function(e) {
     snake.dy = -grid;
     snake.dx = 0;
   }
-  
+
   else if (e.which === 39 && snake.dx === 0) {
     snake.dx = grid;
-    snake.dy = 0; 
+    snake.dy = 0;
   }
 
   else if (e.which === 40 && snake.dy === 0) {
